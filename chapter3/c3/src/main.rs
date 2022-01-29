cc3fbecbe51dcfa7c70058079d8851a21eef0b24
Kaikei-e <e_kaikei@protonmail.com>
2022-01-29 16:56:52 +0900

    Update: Implemented chapter 3-5 list 3-5

--- chapter3/c3/src/main.rs
+++ chapter3/c3/src/main.rs
@@ -2,12 +2,8 @@ fn main() {
     println!("Hello World!!");
 
     let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
 
-    while index < 5 {
-        println!("The value is : {}", a[index]);
-
-        index = index + 1;
+    for element in a.iter() {
+        println!("The value is : {}", element);
     }
-
 }
